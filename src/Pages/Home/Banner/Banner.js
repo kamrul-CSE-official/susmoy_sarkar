fd243dc86087bc4c70ac2460f1cbf282c6e0aea7
Kamrul-Official <kamrul.manzur@gmail.com>
2023-11-07 22:21:41 +0600

    update banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,13 +4,13 @@ import susmoyImg from "../../../Assets/s banner-removebg-preview s (1).png";
 
 export default function Banner() {
   return (
-    <div className="bg-[#fbbd23] min-h-[70vh]">
-      <div className="flex items-center justify-center gap-2 relative">
-        <img className="w-1/4 sticky bottom-0" src={susmoyImg} alt="susmoy" />
+    <div className="bg-[#fbbd23] min-h-[50vh]">
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-2 relative">
+        <img className="w-1/2 lg:w-1/4 bottom-0" src={susmoyImg} alt="susmoy" />
         <div>
           <h2 className="text-4xl font-bold">
             Hi, I am{" "}
-            <span className="text-white">
+            <span className="text-white lg:absolute bottom-48 right-[28rem]">
               <Typewriter
                 options={{
                   strings: [" Susmoy Sarkar", " Susmoy Sarkar"],
